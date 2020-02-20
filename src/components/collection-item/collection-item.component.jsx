00e2329b7e433b5d92e74858ebd4c6df57eb49ad
Yoni Sisso <yonis9@walla.com>
2020-02-20 20:42:30 +0200

    first commit

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
-import { connect } from 'react-redux';
 
 import CustomButton from '../custom-button/custom-button.component';
-import { addItem } from '../../redux/cart/cart.actions';
 
 import './collection-item.styles.scss';
 
@@ -21,18 +19,16 @@ const CollectionItem = ({ item, addItem }) => {
         <span className='name'>{name}</span>
         <span className='price'>{price}</span>
       </div>
-      <CustomButton onClick={() => addItem(item)} inverted>
+      <CustomButton onClick={() =>{ 
+        console.log("addItem",addItem)
+        console.log("item",item)
+        addItem(item)}}
+       inverted>
         Add to cart
       </CustomButton>
     </div>
   );
 };
 
-const mapDispatchToProps = dispatch => ({
-  addItem: item => dispatch(addItem(item))
-});
 
-export default connect(
-  null,
-  mapDispatchToProps
-)(CollectionItem);
+export default CollectionItem;
---
+++ src/components/collections-overview/collections-overview.container.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Query } from 'react-apollo';
+import { gql } from 'apollo-boost';
+
+import Spinner from '../spinner/spinner.component';
+import CollectionOverview from './collections-overview.component'
+
+const GET_COLLECTIONS = gql`
+    {
+        collections {
+            id
+            title
+            items {
+            id
+            name
+            price
+            imageUrl
+            }
+        }
+    }
+`
+
+const CollectionsOverviewContainer = () => (
+    <Query query={GET_COLLECTIONS}>
+        {
+            ({ loading, error, data }) => {
+                console.log({ loading, error, data })
+                if (loading) return <Spinner />
+                return <CollectionOverview collections={data.collections} />
+            }
+        }
+    </Query>
+)
+
+export default CollectionsOverviewContainer;
